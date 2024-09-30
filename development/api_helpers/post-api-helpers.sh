afb48d3cd206d30ab330ef3e25a25a22bca87abb
DoppiaF <francesco.furini@edu.unife.it>
2024-09-30 22:19:52 +0200

    mv api_helpers da sandbox a development

--- development/api_helpers/post-api-helpers.sh
+++ development/api_helpers/post-api-helpers.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 echo "Executing Post-API Helpers"
-~                                   
--- development/api_helpers/pre-api-helpers.sh
+++ development/api_helpers/pre-api-helpers.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
-~                                  
